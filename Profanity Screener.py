07fcaf70d43562d61576c2d1f4f833422b584a74
Martin J Page <martinjpage@outlook.com>
2020-11-04 10:24:23 +0200

    Bug fixes for profanity screener - masking works if profane words is detected by screener

--- Profanity Screener.py
+++ Profanity Screener.py
@@ -35,7 +35,9 @@ def profanityscreen(inputMessage, filterList, mask = False, replacements="$@#*")
         Body: substitutes every second character, excluding
         the first and last characters, of a black word with random symbols
         Output: masked word '''
-        return ''.join([random.choice(replacements) if i % 2 == 1 and i != 0 and i != len (black_word)-1 else black_word[i] for i in range(len(black_word))])
+        return ''.join([random.choice(replacements) if i % 2 == 1 and i != 0 and i != len (black_word)-1 and
+                                                       black_word[i].isalnum() and black_word[i+1].isalnum()
+                                                        else black_word[i] for i in range(len(black_word))])
 
     #censor flagged words
     if mask:
@@ -43,10 +45,10 @@ def profanityscreen(inputMessage, filterList, mask = False, replacements="$@#*")
         message_list = inputMessage.split()
         #take the black words found in the message and convert to regex where the word can be immediately followed
         # by a space, full stop, exlaimation mark or hyphen
-        black_words_regex = [f"{word}[\s\.!\?-]?" for word in black_words]
+        black_words_regex = [f"^(.*-)?\W*({word})\W*(-.+)?$" for word in black_words]
         regexes = [re.compile(word) for word in black_words_regex]
         #rewrite the original message, replacing the characters in words that match the blacklist with random symbols
-        new_message = " ".join([cleaner(w, replacements) if any(regex.match(w.lower()) for regex in regexes) else w for w in message_list])
+        new_message = " ".join([cleaner(w, replacements) if any(regex.search(w.lower()) for regex in regexes) else w for w in message_list])
         return (new_message, black_words)
     else:
         return (inputMessage, black_words)
@@ -55,37 +57,34 @@ def profanityscreen(inputMessage, filterList, mask = False, replacements="$@#*")
 ### Tests ###
 inMsg1 = "Bastard begot, bastard instructed, bastard in mind,... in everything illegitimate."
 print(profanityscreen(inMsg1, blacklist, mask=True))
-# masks identified word regardless of capital letters at the start
 
 inMsg2 = "Son and heir of a mongrel bitch."
 print(profanityscreen(inMsg2, blacklist, mask=True))
-# masks identified word regardless of punctuation at the end
 
 inMsg3 = "Thou bitch-wolf's son!"
 print(profanityscreen(inMsg3, blacklist, mask=True))
-# masks identified term if the profane terms is at the beginning of hyphen
 
 inMsg4 = "You are a motherfucker"
 print(profanityscreen(inMsg4, blacklist, mask=True))
-## masks identified word
 
 inMsg5 = "You are a motherfucker!"
 print(profanityscreen(inMsg5, blacklist, mask=True))
-## masks identified word BUT recognises puntuctaion mark as final character
-## intention: keep first and last character of swear word and mask every other letter
 
 inMsg6 = "You are a mother-fucker!"
 print(profanityscreen(inMsg6, blacklist, mask=True))
-## identifies word but does not mask it because the word is after the hyphen
-## intention: mask compound swear word if the term is the first or second part of the hyphenated term
-## currently only masks if swaer word is the first part (ie before hyphen)
 
-inMsg7 = "You bastard!!"
+inMsg7 = "You $%&*bastard!$%&*'"
 print(profanityscreen(inMsg7, blacklist, mask=True))
-## punctuation marks are also substituted with other symbols when in multiple
+
+inMsg8 = "The Scunthorpe problem is that our town's name is censored because it contains the substring 'cunt'. No fair!"
+print(profanityscreen(inMsg8, blacklist, mask=True))
+# ERROR: does not pick up word in quotes ('') - because of word_tokenizer
+
+inMsg9 = "The Scunthorpe problem is that our town's name is censored because it contains the substring cunt. No fair!"
+print(profanityscreen(inMsg9, blacklist, mask=True))
 
 ##improvement: currently the black-word output list is only the base terms; can improve by
 ## retunring a black-word output list that includes novel hyphenated swear words
 
-##improvement: search for 2+ word terms - need different search strategy to scan blacklist over message rather
+##improvement + fix: change search stragtegy to search profane terms of multiple length - scan blacklist over message rather
 ## than message (broken into n-grams) over blacklist
